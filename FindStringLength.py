cf69489911edf6a1dc58c03b298716fac2b8d38d
MANIKANDAN K <160108937+MANIKANDAN-17K@users.noreply.github.com>
2025-01-06 23:45:47 +0530

    Add files via upload

---
+++ FindStringLength.py
@@ -0,0 +1,5 @@
+   
+def a_function( str1 ):        
+    return len(str1)        
+print( "Length of the string Functions is: ", a_function( "Functions" ) )    
+print( "Length of the string Python is: ", a_function( "Python" ) ) 
