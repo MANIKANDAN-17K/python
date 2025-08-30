2cecaf97eda75dab85f08877799af8742c55b4e1
MANIKANDAN K <160108937+MANIKANDAN-17K@users.noreply.github.com>
2025-08-30 23:30:20 +0530

    Create PrimeChecker.py

---
+++ PrimeChecker.py
@@ -0,0 +1,21 @@
+import math
+
+def isPrime(n):
+    if n < 2:
+        return False
+    if n == 2:
+        return True
+    if n % 2 == 0:
+        return False
+        
+    for i in range (3,int(math.sqrt(n))+1,2):
+        if n % i == 0:
+            return False
+            
+    return True
+    
+n = int(input("Enter the number is prime or not : "))
+if isPrime(n) :
+    print(f" the given number {n} is prime")
+else:
+    print(f"the given number {n} is not prime")
