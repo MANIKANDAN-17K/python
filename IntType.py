b020d01d4201750357bc9d7369d4a90c21155e4d
MANIKANDAN K <160108937+MANIKANDAN-17K@users.noreply.github.com>
2025-09-13 20:56:25 +0530

    Add files via upload

---
+++ IntType.py
@@ -0,0 +1,19 @@
+# now we are going to see the integer datatype and its methods
+x = 10
+y = -15
+
+print(abs(y)) # abs()
+print(pow(4,2)) # pow(x,y)
+print(round(3.456,2)) #rond()
+print(round(2.45643)) #round()
+print(divmod(10,3)) #div and quotient and remainder
+print(max(3,6,3,2,4)) #max value
+print(min(3,54,5,3,2))#min value
+print(sum([3,4,5,6]))# sum
+print(bin(10)) # binary
+print(oct(10))
+print(hex(234))
+print(int("1234"))
+print(type(x))
+
+
