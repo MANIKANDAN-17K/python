3aa009c2948206b872c20a302aeeb5d62a6b7305
MANIKANDAN K <160108937+MANIKANDAN-17K@users.noreply.github.com>
2025-09-13 20:57:05 +0530

    Add files via upload

---
+++ Operators.py
@@ -0,0 +1,25 @@
+a = 7
+b = 3
+print(a + b, a - b, a * b, a / b, a // b, a % b, a ** b)
+
+
+x = 10
+x += 5
+print(x)  # 15
+
+
+a = True
+b = False
+print(a and b, a or b, not a)  # False True False
+
+#boolean
+
+print(3&4,5^3,~2,5<<1,2>>5)
+
+x = [1,2,3]
+y = [1,2,3]
+print(x == y)  # True (values equal)
+print(x is y)  # False (different objects)
+print("hlel")
+
+print(1 in x)
